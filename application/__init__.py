5e99faeb2735481d3dc0a51c7d27a78cc7ee7421
yennhan <leowyennhan@gmail.com>
2019-01-08 03:01:31 +0800

    Reduce page load and added minified assets and js files

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask,request,redirect,url_for,make_response
 from flask_cors import CORS
 from flask_compress import Compress
+from flask_assets import Environment,Bundle
 from .api.authentication.routes import login_manager
 from .api.authentication.model import check_refresh_token,check_valid_token,refresh_token
 from .api.database.model import dynamodb_user_upload,get_all_items
@@ -10,8 +11,18 @@ from application import api
 from application import site
 
 
+js=Bundle('js/functions/form.js','js/functions/facebook_login.js','js/plugins.js','js/isotope.pkgd.min.js',
+          'js/jquery.easing.1.3.js','js/jquery.countdown.min.js','js/jquery.magnific-popup.min.js','js/owl.carousel.min.js','js/parallax.min.js','js/jquery.easypiechart.min.js','js/spectragram.min.js',
+          'js/jquery.waypoints.min.js','js/scrollax.js','js/functions/load_campaign.js','js/main.js',output='gen/main.js', filters="jsmin")
+
+css=Bundle('css/iconfont.css','css/font-awesome.min.css','css/bootstrap.min.css','css/isotope.css','css/magnific-popup.css','css/owl.carousel.min.css',
+           'css/owl.theme.default.min.css','css/woocommerce.css','css/plugins.css','css/plugins.css','css/style.css','css/responsive.css',output='gen/main.css',filters="cssmin")
 app = Flask(__name__)
 login_manager.init_app(app)
+assets = Environment(app)
+
+assets.register('main_css',css)
+assets.register('main_js',js)
 
 CORS(app)
 Compress(app)
